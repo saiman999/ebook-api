962efbca02793008cc70b7e3dfdf8b4e0f3928c7
saiman999 <mzaf04@gmail.com>
2021-08-04 04:38:19 +0700

    KK4C

---
+++ resources/views/newHome.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Repo Github</title>
+    </head>
+    
+    <body>
+        <h1>https://github.com/saiman999/ebook-api.git</h1>
+        <p>Link Repository Github</p>
+    </body>
+</html>
